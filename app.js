d3aef671d1dec57db69be7dc38d8723539ba9465
ayaazzam24 <ayaazzam0101@gmail.com>
2021-02-11 14:42:15 +0200

    solve the problem 3

--- app.js
+++ app.js
@@ -41,22 +41,34 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+ncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+/*Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
-
+Test this function by hand in the console to get it working, and when you think it is finished, u
 // Write your code here
-function sumAndMultiply(a, b, c) {
+
+
+
+
+*/ function sumAndMultiply(a, b, c) {
+  let m=  sum(4,7);
+  let y = sum(m[0],5)
+ console.log (y[0]);
+  let i=  multiply(4,7);
+  let j = multiply(i[0],5)
+ console.log (j[0]);
+  return [ y[0] , j[0] ,`${a} and ${b} and ${c} sum to ${y[0]}.` ,`The product of ${a} and ${b} and ${c} is ${j[0]}.`];
+ 
   //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
